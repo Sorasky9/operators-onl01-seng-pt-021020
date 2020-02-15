30630a53c2ec8fa1b5ccc8f699d11df42671b1c8
Malcom Fetus <sora.vista@gmail.com>
2020-02-15 22:59:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,25 +1,14 @@
-require 'pry'
 def unsafe?(speed)
-# binding.pry
-if speed > 60
-  true
-elsif speed < 40
-true
-else
- if speed >= 40 && speed <= 60
-  false
+  if speed > 60 || speed < 40
+    true
+  else
+    false
 
-end
-end
+  end
 end
 
 def not_safe?(speed)
-    (speed > 60) ? true
 
-     (speed < 40) ? true 
-    true
-  else (speed >= 40 && speed <= 60) ? false : true
-    false
 
-end
+
 end
